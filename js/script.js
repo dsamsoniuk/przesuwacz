efbe423c723a296827caf8e4a2345d625df0b7f6
damian12345 <d.samsoniuk@protonmail.com>
2018-06-12 11:11:39 +0200

    akcje na poszczegolne objekty i gify

--- js/script.js
+++ js/script.js
@@ -7,25 +7,60 @@ var size_block  = 20; // in px
 
 var map = [
     [48,8, "stone"],[68,8, "water"],
-    [8,68, "stone"],
+    [8,68, "stone"],[108,8, "gold"],
     [8,48, "stone"]
 ];
 
 var block = {
-    over : function (top,left,type) {
+    over : function (top,left) {
 
         for (i = 0; i < map.length; i++) {
             if (map[i][0] == left && map[i][1] == top) {
-                this.onIn();
-                return true;
+                var id      = map[i][2] + "_" + i;
+                var type    = map[i][2];
+                return this.onIn(id, type);
             }
         }
         return false;
     },
-    onIn : function () {
-
+    onIn : function (id, type) {
+        if (type == "stone") {
+            var result = this.source.stone(id);
+            this.setInfo(result.info);
+            return result.res;
+        }       
+        if (type == "water") {
+            var result = this.source.water(id);
+            this.setInfo(result.info);
+            return result.res;
+        }
+        if (type == "gold") {
+            var result = this.source.gold(id);
+            this.setInfo(result.info);
+            this.setScore(result.score);
+            return result.res;
+        }
+        return true;
+    },
+    source : {
+        stone : function(id) {
+            return { "info" : "Troche za wysoko nie da się wspiąć na ten głaz", "res" : true };
+        },
+        water : function(id) {
+            return { "info" : "Zimna woda, nie możesz wejść", "res" : true };
+        },
+        gold : function(id) {
+            $("#" + id).remove();
+            return { "info" : "Jestes w złocie", "res" : false, "score" : 1 };
+        },
+    },
+    setInfo  : function(txt){
+        $("#info").html("Info : " + txt);
+    },
+    setScore : function (num) {
+        var currentScore = parseInt($("#score").html());
+        $("#score").html(currentScore + num);
     }
-
 }
 
 function placeBox(top,left) {
@@ -50,6 +85,7 @@ function placeBox(top,left) {
 }
 
 $( "body" ).on( "keydown", function( event ) {
+
     if (event.which == 37) { //lewa strzalka
         placeBox(0,-1);
     }
@@ -67,9 +103,10 @@ $( "body" ).on( "keydown", function( event ) {
 function generateMap(map) {
     var i;
     for (i = 0; i < map.length; i++) {
-        var box_block   = "<div class=\" block "+map[i][2]+"\"></div>";
+        var id          = map[i][2] + "_" + i;
+        var box_block   = "<div class=\" block " + map[i][2] + "\" id=\"" + id + "\"></div>";
         var b           = $(box_block).css({top : map[i][1], left : map[i][0]});
         $('#area').append( b.prop("outerHTML") );
     }
 }
-generateMap(map)
+generateMap(map);
